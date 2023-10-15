ef8bdf497a47e2495366f898dcd914823c7a92b8
RobinZSun <robinsun0126@gmail.com>
2023-10-15 23:34:39 +0100

    Updated by 15 Oct

--- BIPPdCVnet.R
+++ BIPPdCVnet.R
@@ -1,2 +1,15 @@
-#Test start
-setwd(C:\R wd\BIPP_dCVnet)
+setwd("C:\\R wd\\BIPP_dCVnet")
+
+##load necessary packages####
+library(tidyverse)
+
+# Inspect Data#####
+load("data/zpsypred.RData")
+# Numeric variables apart from IMD and Age were z-scored.
+# The z-scores are based on this exact sample.
+
+# Check missing values#
+mice::md.pattern(dt)
+mice::md.pattern(zdt)
+#no missing values
+
